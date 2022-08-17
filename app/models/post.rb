ff3a2af64d6145c6c7bc592cfd76965d5c3c9de0
Christopher Callender <christophercallender0000@gmail.com>
2022-08-17 19:16:08 -0400

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :validate_title
+
+  def validate_title
+    if title.present?
+      unless title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+        errors.add(:title, "clickbait")
+      end
+    end
+  end
 end
